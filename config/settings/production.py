903b51a9079b1bc603f71adb9dfb61e9cb6237d5
Andri Janusson <andrijan@gmail.com>
2017-03-08 21:13:45 +0000

    Add ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,10 @@
 import dj_database_url
 from .common import *  # noqa
 
+ALLOWED_HOSTS = env.list(  # noqa
+    'DJANGO_ALLOWED_HOSTS',
+    default=['fiver-dynastry.herokuapp.com']
+)
 INSTALLED_APPS += ("gunicorn", )  # noqa
 
 db_from_env = dj_database_url.config()
