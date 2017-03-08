b780d012ca4944d193a9eaedd62367c424195143
Andri Janusson <andrijan@gmail.com>
2017-03-08 17:09:17 +0000

    Add dj-database-url

--- fiver/fiver/settings.py
+++ fiver/fiver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -69,6 +70,7 @@ WSGI_APPLICATION = 'fiver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
     'default': {
@@ -76,6 +78,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
