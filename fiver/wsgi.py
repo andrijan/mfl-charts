65ce3ba394db37e340e48b6f593c9cd9b538bfb0
Andri Janusson <andrijan@gmail.com>
2017-03-08 17:19:42 +0000

    Update wsgi file

--- fiver/wsgi.py
+++ fiver/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiver.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fiver.fiver.settings")
 
 application = get_wsgi_application()
