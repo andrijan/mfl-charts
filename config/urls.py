7de2ac1f529bdb693a470207e16b74d3c0fd9fdb
Andri Janusson <andrijan@gmail.com>
2017-03-08 21:50:16 +0000

    Fuck dis shit

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
+
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('fiver.apps.stats.urls')),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
