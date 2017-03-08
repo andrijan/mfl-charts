2930136ff4f274f31c8a549f508b6c14619c846e
Andri Janusson <andrijan@gmail.com>
2017-03-08 17:30:03 +0000

    Test path mixup

--- fiver/fiver/settings.py
+++ fiver/fiver/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'apps.stats',
-    'apps.mfl',
+    'fiver.apps.stats',
+    'fiver.apps.mfl',
 ]
 
 MIDDLEWARE = [
